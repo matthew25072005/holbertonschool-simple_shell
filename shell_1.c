c3787e6a60c63d1d501587591d80aac7acd2e2d0
matthew25072005 <8855@holbertonstudents.com>
2024-04-17 17:16:27 +0000

    shell

--- shell_1.c
+++ shell_1.c
@@ -1,57 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/wait.h>
-
-#define MAX_CMD_LEN 256
-#define PROMPT "#cisfun$ "
+#include <unistd.h>
 
 int main(void)
 {
-    char cmd[MAX_CMD_LEN];
-    pid_t pid;
-    int status;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
 
-    while (1)
-    {
-        printf(PROMPT);
-        if (fgets(cmd, sizeof(cmd), stdin) == NULL)
-        {
-            printf("\n");
-            exit(0);
-        }
-
-        cmd[strlen(cmd) - 1] = '\0';
-
-        if (strcmp(cmd, "exit") == 0)
-        {
-            exit(0);
-        }
-
-        pid = fork();
-        if (pid < 0)
-        {
-            perror("Fork failed");
-            exit(1);
-        }
-        else if (pid == 0)
-        {
-            char *argv[2];
-            argv[0] = cmd;
-            argv[1] = NULL;
-
-            if (execve(cmd, argv, NULL) == -1)
-            {
-                perror(cmd);
-            }
-            exit(0);
-        }
-        else
-        {
-            wait(&status);
-        }
-    }
-
-    return 0;
+	while (1)
+	{
+		printf("#cisfun$ ");
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+		{
+			free(line);
+			return (0);
+		}
+		line[read - 1] = '\0';
+		if (access(line, X_OK) == 0)
+		{
+			system(line);
+		}
+		else
+		{
+			printf("%s: No such file or directory\n", line);
+		}
+	}
+	free(line);
+	return (0);
 }
