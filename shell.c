348509d0a014e337412c52e4d035214ad68d4b1b
matthew25072005 <8855@holbertonstudents.com>
2024-04-23 16:04:06 +0000

    shell

--- shell.c
+++ shell.c
@@ -2,53 +2,60 @@
 
 int main(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	char *token;
-	char *args[ARG_MAX];
-	int i;
-	pid_t pid;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    char *token;
+    char *args[ARG_MAX];
+    int i;
+    pid_t pid;
 
-	while (1)
-	{
-		printf("#cisfun$ ");
-		read = getline(&line, &len, stdin);
-		if (read == -1)
-		{
-			free(line);
-			return (0);
-		}
-		line[read - 1] = '\0';
+    while (1)
+    {
+        printf("#cisfun$ ");
+        fflush(stdout);
+        read = getline(&line, &len, stdin);
+        if (read == -1)
+        {
+            free(line);
+            exit(EXIT_SUCCESS);
+        }
+        line[read - 1] = '\0';
 
-		i = 0;
-		token = strtok(line, " ");
-		while (token != NULL)
-		{
-			args[i] = token;
-			i++;
-			token = strtok(NULL, " ");
-		}
-		args[i] = NULL;
+        if (strcmp(line, "exit") == 0)
+        {
+            free(line);
+            exit(EXIT_SUCCESS);
+        }
 
-		pid = fork();
-		if (pid == 0)
-		{
-			if (execvp(args[0], args) == -1)
-			{
-				printf("%s: No such file or directory\n", line);
-				exit(EXIT_FAILURE);
-			}
-		}
-		else if (pid < 0)
-		{
-			perror("Error");
-		}
-		else
-		{
-			while (wait(NULL) != pid);
-		}
-	}
-	free(line);
-	return (0);
+        i = 0;
+        token = strtok(line, " ");
+        while (token != NULL)
+        {
+            args[i] = token;
+            i++;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;
+
+        pid = fork();
+        if (pid == 0)
+        {
+            if (execve(args[0], args, NULL) == -1)
+            {
+                perror(line);
+                _exit(EXIT_FAILURE);
+            }
+        }
+        else if (pid < 0)
+        {
+            perror("Error");
+        }
+        else
+        {
+            while (wait(NULL) != pid);
+        }
+    }
+    free(line);
+    return (0);
 }
