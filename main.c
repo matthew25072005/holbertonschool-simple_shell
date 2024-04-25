eb9773c948222d875f54890df28dfc6e15f216c1
matthew25072005 <8855@holbertonstudents.com>
2024-04-25 23:55:16 +0000

    shell

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include "shell.h"
 
+/**
+ * main - Entry point for the shell
+ *
+ * This function is the entry point for the shell program. It reads commands
+ * from the standard input, parses them into arguments, and then executes the
+ * command. It continues to do this in a loop until the user types "exit".
+ *
+ * Return: 0 on success, otherwise the exit status of the last command executed
+ */
+
 int main(void)
 {
 	char *line = NULL;
--- parse_command.c
+++ parse_command.c
@@ -1,5 +1,15 @@
 #include "shell.h"
 
+/**
+* parse_command - Parses a command line into arguments
+* @line: The command line to parse
+* @args: The array to store the parsed arguments
+*
+* This function takes a command line and parses it into separate arguments.
+* The arguments are stored in the provided array. The array will be NULL
+* terminated.
+*/
+
 void parse_command(char *line, char **args)
 {
 	int i = 0;
